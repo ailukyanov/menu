d6101e822a518e6fcd39fe932f0253ac7cf239c2
ailukyanov <ailukyanov90@gmail.com>
2019-08-27 20:59:51 +0300

    test commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,27 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
     return gulp.src('./scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
-});
+// Starts a BrowerSync instance
+gulp.task('default', gulp.series('sass', function(){
+    browserSync.init({
+        server: ".",
+        port: 4000
+    });
+
+    gulp.watch("*.html").on("change", browserSync.reload);
+    gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+}));
+
+
